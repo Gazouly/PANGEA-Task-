971e7a2153ba9d14fefa374963cd66ae0deaf6bd
Gazouly <m.algazouly96@gmail.com>
2020-06-27 14:28:37 +0200

    Change the app folder structure
    Created views directory to handle any new view that will be made in the future

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,14 @@
 <template>
-  <v-app>
-    <v-toolbar
-      app
-    >
-      <v-toolbar-title v-text="title"/>
-    </v-toolbar>
-    <v-content>
-      <v-layout
-        align-center
-        fill-height
-        justify-center
-      >
-        <v-flex xs6>
-          <TodoForm />
-          <TodoList/>
-        </v-flex>
-      </v-layout>
-    </v-content>
-  </v-app>
+  <TodoView />
 </template>
 
 <script>
-import TodoForm from '@/components/todo-form'
-import TodoList from '@/components/todo-list'
+import TodoView from '@/views/Todo'
 
 export default {
   name: 'App',
   components: {
-    TodoForm,
-    TodoList,
+    TodoView,
   },
-  data () {
-    return {
-      title: 'Vue Todos'
-    }
-  }
 }
 </script>
---
+++ src/views/Todo.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title v-text="title" />
+    </v-toolbar>
+    <v-content>
+      <v-layout 
+        align-center 
+        fill-height 
+        justify-center>
+        <v-flex xs8>
+          <TodoForm />
+          <TodoList />
+        </v-flex>
+      </v-layout>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import TodoForm from "@/components/todo-form";
+import TodoList from "@/components/todo-list";
+
+export default {
+  name: "TodoView",
+  components: {
+    TodoForm,
+    TodoList
+  },
+  created() {
+    this.$store.dispatch('INIT_TODO')
+  },
+  data() {
+    return {
+      title: "Vue Todos"
+    };
+  }
+};
+</script>
