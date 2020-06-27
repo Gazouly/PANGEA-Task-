891777d2ab384360da3f8a6c06a0fafe89fa8e5f
Gazouly <m.algazouly96@gmail.com>
2020-06-27 14:42:24 +0200

    Implemented todo form and list
    added logic and used vuex actions to receive new todos and add them to state

--- src/components/todo-form.vue
+++ src/components/todo-form.vue
@@ -3,11 +3,28 @@
     label="Todo"
     placeholder="What do you have to do?"
     solo
+    v-model="title"
+    @keyup.enter="createTodo"
   />
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    createTodo() {
+      let todo = {
+        title: this.title,
+        completed: false
+      }
+      this.$store.dispatch('CREATE_TODO', todo)
+      this.title = ''
+    }
+  }
 }
 </script>
 
--- src/components/todo-list.vue
+++ src/components/todo-list.vue
@@ -1,7 +1,30 @@
 <template lang="html">
-  <div>
-    <TodoItem class="mb-3" />
-    <TodoItem class="mb-3"/>
+  <div class="d-flex justify-space-between ">
+    <div class="not-completed">
+      <h2 
+        class="mb-2" 
+        style="text-align: center">Ongoing Tasks</h2>
+      <TodoItem 
+        v-for="todo in todos" 
+        :key="todo.id" 
+        :todo="todo"
+        class="mb-3" />
+    </div>
+    <v-divider
+      class="mx-4"
+      vertical
+    />
+    <div class="completed">
+      <h2 
+        class="mb-2" 
+        style="text-align: center">Completed Tasks</h2>
+      <TodoItem 
+        v-for="todo in completed" 
+        :key="todo.id" 
+        :todo="todo"
+        class="mb-3" 
+        :completed="true"/>
+    </div>
   </div>
 </template>
 
@@ -10,7 +33,15 @@ import TodoItem from '@/components/todo-item';
 
 export default {
   components: {
-    TodoItem,
+    TodoItem
+  },
+  computed: {
+    todos() {
+      return this.$store.getters.ongoingTasks
+    },
+    completed() {
+      return this.$store.getters.completedTasks
+    }
   }
 }
 </script>
